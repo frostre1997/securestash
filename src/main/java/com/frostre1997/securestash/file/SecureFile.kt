a46c2a3cf7f190f778279bcc4e35f4490db3d5ea
Frostre_ <n9043395@gmail.com>
2026-08-11 16:23:13 +0200

    implement file encryption and decryption extensions

---
+++ src/main/java/com/frostre1997/securestash/file/SecureFile.kt
@@ -0,0 +1,27 @@
+package com.frostre1997.securestash.file
+
+import android.content.Context
+import com.yourname.securestash.crypto.CryptoManager
+import java.io.File
+
+/**
+ * Extension function to encrypt a file.
+ */
+fun File.encrypt(context: Context, outputFile: File) {
+    CryptoManager.encryptFile(context, this, outputFile)
+}
+
+/**
+ * Extension function to decrypt a file.
+ */
+fun File.decrypt(context: Context, outputFile: File) {
+    CryptoManager.decryptFile(context, this, outputFile)
+}
+
+/**
+ * Extension to encrypt a file in-place (creates a .encrypted version).
+ */
+fun File.encryptInPlace(context: Context) {
+    val output = File(this.parent, "${this.name}.encrypted")
+    CryptoManager.encryptFile(context, this, output)
+}
